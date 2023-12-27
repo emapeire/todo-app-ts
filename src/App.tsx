7f531d7acd90200f2aabd6ec96eda53da597697c
Emanuel Peire <emapeire@outlook.com>
2023-12-27 18:15:16 -0300

    Add filter functionality and footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import { mock } from './mock'
 import Todos from './components/Todos'
-import { CompletedProps, TodoId } from './types'
+import { CompletedProps, FilterValues, TodoId } from './types'
+import { TODO_FILTERS } from './constants'
+import Footer from './components/Footer'
 
 export default function App() {
   const [todos, setTodos] = useState(mock)
+  const [filterSelected, setFilterSelected] = useState<FilterValues>(
+    TODO_FILTERS.ALL
+  )
 
   const handleRemoved = ({ id }: TodoId) => {
     const newTodos = todos.filter((todo) => todo.id !== id)
@@ -21,12 +26,26 @@ export default function App() {
     setTodos(newTodos)
   }
 
+  const handleFilterChange = (filter: FilterValues) => {
+    setFilterSelected(filter)
+  }
+
+  const activeCount = todos.filter((todo) => !todo.completed).length
+  const completedCount = todos.filter((todo) => todo.completed).length
+
   return (
     <div className='todoapp'>
       <Todos
         onCompleted={handleCompleted}
         onRemoved={handleRemoved}
         todos={todos}
+      />
+      <Footer
+        activeCount={activeCount}
+        completedCount={completedCount}
+        onClearCompleted={() => {}}
+        filterSelected={filterSelected}
+        onFilterChange={handleFilterChange}
       />
     </div>
   )
